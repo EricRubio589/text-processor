1759399b592adcb67ce62ba6c80aedfee4eaf700
Eric Rubio <eric.rubio.services@gmail.com>
2019-06-14 01:14:16 -0500

    mostly functional app

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const NLP_API_KEY = 'AIzaSyBy9CeoxXbMk-1CNSOOBn-ATQMPP-yayMs';
 const NLP_API_URL = 'https://language.googleapis.com/v1beta2/documents:';
 const NLP_METHOD = 'annotateText';
+var entitiesWithEnoughSalience = [];
+var reducedText = [];
 
 
 function analyzeEntitySentiment(text){
@@ -50,34 +52,43 @@ function listenForFormSubmit() {
 
 }
 
+function getEntitiesWithSalience(array) {
+  for (let i=0; i < Object.keys(array.entities).length; i++ ) {
+    if ((array.entities[i].salience * 100) >= 2) {
+    entitiesWithEnoughSalience.push(array.entities[i].name)
+    }
+  }
+  filterSentencesWithSalienceWords(entitiesWithEnoughSalience,array);
+}
+
+function filterSentencesWithSalienceWords(words,array) {
+  let entitiesAlreadyUsed = [];
+  for(let i=0; i < Object.keys(array.sentences).length; i++) {
+    for (let j=0; j < words.length; j++) {
+      if (array.sentences[i].text.content.includes(words[j]) 
+      && !reducedText.includes(array.sentences[i].text.content) 
+      && !entitiesAlreadyUsed.includes(words[j])) {
+        console.log(entitiesWithEnoughSalience);
+        reducedText.push(array.sentences[i].text.content);
+        entitiesAlreadyUsed.push(words[j]);
+      }
+    }  
+  } 
+}
+
+//this is the function that runs after clicking on analize, it calls getEntitiesWithSalience//
 function listenForDisplayResultsSubmit(data) {
   $('.displayResultsButton').click(function handleData(event) {
-      event.preventDefault();
-      var reducedText = [];
-
-        /*for (let i=0; i < Object.keys(data.sentences).length; i++) {
-          if ((data.sentences[i].sentiment.score * 100) >= 50 
-          || (data.sentences[i].sentiment.score * 100) <= -50) 
-          {
-            reducedText.push(data.sentences[i].text.content + " ");
-            console.log(i); 
-          }
-        }
-        */
-        for (let i=0; i < Object.keys(data.entities).length; i++) {
-
-          if ((data.entities[i].salience * 100) >= 1) {
-            for (let i=0; i < Object.keys(data.sentences).length; i++)
-          } 
-
-          if ("wikipedia_url" in data.entities[i].metadata && (data.entities[i].salience * 100) >= 1) {
-            $('.resultsList').show();
-            $('.resultsList').append(`<li><a href="${data.entities[i].metadata.wikipedia_url}" target="_blank">${data.entities[i].name}</a></li>`)
-          }
-        }
-        
-      $('.resultsDisplay').append(`<div>${reducedText}</div>`).val();
-      console.log(data);
+    event.preventDefault();
+    getEntitiesWithSalience(data)
+    for (let i=0; i < Object.keys(data.entities).length; i++) {
+      if ("wikipedia_url" in data.entities[i].metadata && (data.entities[i].salience * 100) >= 1) {
+        $('.resultsList').show();
+        $('.resultsList').append(`<li><a href="${data.entities[i].metadata.wikipedia_url}" target="_blank">${data.entities[i].name}</a></li>`)
+      }
+    }
+    $('.resultsDisplay').append(`<div>${reducedText}</div>`).val();
+    console.log(reducedText);
   });
 }
 
